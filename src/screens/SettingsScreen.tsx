ac4b6a4528dbb11be2141d0f2a29a650d7535ac9
TheXires <xires.dev@gmail.com>
2023-10-19 22:30:56 +0200

    Add test setting with switch

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,22 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useTheme } from 'react-native-paper';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { List, Switch, useTheme } from 'react-native-paper';
 
 export default function SettingsScreen() {
   const theme = useTheme();
   const { t } = useTranslation();
 
+  const [toggled, setToggled] = useState<boolean>(false);
+
   return (
     <View style={styles.container}>
       <Text style={{ color: theme.colors.onSurface }}>{t('SettingsScreen.title')}</Text>
+      <ScrollView>
+        <List.Item
+          title="Test"
+          right={() =>
+            Switch({
+              onChange: () => setToggled((prev) => !prev),
+              value: toggled,
+            })
+          }
+          style={{ flex: 1, justifyContent: 'space-between' }}
+        />
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     flex: 1,
     justifyContent: 'center',
   },
